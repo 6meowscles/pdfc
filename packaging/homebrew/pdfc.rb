4e0e7c73212a04a4939db795fee56f79549e63e4
6meowscles <mharshita2309@gmail.com>
2026-09-16 10:34:02 +0530

    Record the v0.3.2 source checksums

--- packaging/homebrew/pdfc.rb
+++ packaging/homebrew/pdfc.rb
@@ -4,7 +4,7 @@ class Pdfc < Formula
   desc "Local PDF converter: images, text, markdown, HTML and office formats"
   homepage "https://github.com/6meowscles/pdfc"
   url "https://github.com/6meowscles/pdfc/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "PLACEHOLDER_REGENERATE_AFTER_TAGGING"
+  sha256 "37154698b056ec2d060802778decf57c9bf2b4bd6bc20cf5a055d9badd880154"
   license "AGPL-3.0-or-later"
   head "https://github.com/6meowscles/pdfc.git", branch: "main"
 
